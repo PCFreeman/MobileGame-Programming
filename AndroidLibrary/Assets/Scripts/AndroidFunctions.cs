dae56c37c34c51f77ba95b40347137cdd8901a18
PCFreeman <freemanfrank96@gmail.com>
2018-11-01 20:42:22 -0700

    Ooof

---
+++ AndroidLibrary/Assets/Scripts/AndroidFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AndroidFunctions : MonoBehaviour {
+
+    public void ShowToastMessage(string message)
+    {
+        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+
+        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+        unityActivity.Call("runOnUiThread",new AndroidJavaRunnable(()=> {
+            Debug.Log("Running on UI thread");
+
+            AndroidJavaObject applicationContext = unityActivity.Call<AndroidJavaObject>("getApplicationContext");
+
+            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+
+            AndroidJavaObject javaString = new AndroidJavaObject("java.lang.String",message);
+
+            AndroidJavaObject toastInstance = toastClass.CallStatic<AndroidJavaObject>("makeText",applicationContext,message,toastClass.GetStatic<int>("LENGTH_SHORT"));
+
+            toastInstance.Call("show");
+        }));
+    }
+	
+		
+}
